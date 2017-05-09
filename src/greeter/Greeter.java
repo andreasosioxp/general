c957de433245001c81a8a2c37089f7a1fd120b50
XPeppers <xpeppers@XPepperss-MacBook-Pro.local>
2017-05-09 11:12:34 +0200

    primo commit di qua

--- src/greeter/Greeter.java
+++ src/greeter/Greeter.java
@@ -2,10 +2,8 @@ package greeter;
 
 public class Greeter {
 	public Greeter(DateProvider dateProvider, MailSender mailSender, EmployeeListProvider employeeListProvider) {
-		// TODO Auto-generated constructor stub
 	}
 
 	public void greet() {
-		// TODO Auto-generated method stub
 	}
 }
